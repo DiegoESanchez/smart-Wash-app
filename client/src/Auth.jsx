17fb9f64dd2829709ab60beefa0a2e7ff8f0aa3b
Diego Sanchez <Dsanchez0522@gmail.com>
2018-11-27 21:39:29 -0600

    modify the client folder and worked on the Auth.jsx

---
+++ client/src/Auth.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import {Redirect} from 'react-router-dom';
+//import fire from "./components/fire.jsx";
+import Login from "./components/Login.jsx";
+import SignUp from "./components/signUp.jsx";
+import App from './index.jsx';
+import Home from './userHome.jsx'
+
+
+
+
+// import Login from './Login';
+
+
+
+class Auth extends Component {
+  constructor(props) {
+    super(props);
+    // this.state = {
+    //user: null,
+    //account: udefined
+    //}
+    //this,authListener = this authListener.bind(this);
+  }
+
+  componentDidMount(){
+    this.props.authListener();
+  }
+
+  // authListener() {
+  //   fire.auth().onAuthStateChanged((user) => {
+  //     console.log(user);
+  //     if (user) {
+  //       this.setState({ user });
+  //       localStorage.setItem('user', user.uid);
+  //     } else {
+  //       this.setState({ user: null });
+  //       localStorage.removeItem('user');
+  //     }
+  //   });
+  // }
+    render() {
+      return (
+        <div>
+            <h1>  &#9885; SmartWash </h1>
+            {this.props.state.user ? (<Redirect to= "/micuenta"/>) : (<Login/>)}
+            </div>
+          )}
+        }
+export default Auth;
+
+//<Home user={this.props.state.user}/>
